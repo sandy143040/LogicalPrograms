3f0e97246743aa42a967d9fc0687767931c96d12
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-20 06:57:33 +0530

    [Sandeep] Adding Welcome Message

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming Problem");
+            Console.ReadLine();
+                
+        }
+    }
+}
