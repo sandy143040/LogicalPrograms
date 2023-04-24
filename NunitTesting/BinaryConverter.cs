46166239eb276e701eccad7f7f25dab0911074c3
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-25 01:11:04 +0530

    [Sandeep] Modify Converting Decimal to Binary and Swap nibbles

--- NunitTesting/BinaryConverter.cs
+++ NunitTesting/BinaryConverter.cs
@@ -8,25 +8,50 @@ namespace NunitTesting
 {
     internal class BinaryConverter
     {
-        public static string ToBinary(int decimalNumber)
+        public static string ToBinary(int number)
         {
-            int remainder;
-            string binaryString = "";
+            int quotient;
+            string binary = string.Empty;
 
-            while (decimalNumber > 0)
+            while (number > 0)
             {
-                remainder = decimalNumber % 2;
-                binaryString = remainder.ToString() + binaryString;
-                decimalNumber /= 2;
+                quotient = number / 2;
+                binary = (number % 2) + binary;
+                number = quotient;
             }
 
-            // Pad the binary string with 0s to ensure it has 32 bits
-            while (binaryString.Length < 32)
+            return binary;
+        }
+
+        public static int SwapNibbles(string binary)
+        {
+            int[] nibbles = new int[8];
+            for (int i = 0; i < binary.Length; i++)
             {
-                binaryString = "0" + binaryString;
+                nibbles[i] = int.Parse(binary[i].ToString());
             }
 
-            return binaryString;
+            // Swap nibbles
+            for (int i = 0; i < 4; i++)
+            {
+                int temp = nibbles[i];
+                nibbles[i] = nibbles[i + 4];
+                nibbles[i + 4] = temp;
+            }
+
+            // Calculate decimal value of swapped nibbles
+            int value = 0;
+            for (int i = nibbles.Length - 1; i >= 0; i--)
+            {
+                value += nibbles[i] * (int)Math.Pow(2, nibbles.Length - 1 - i);
+            }
+
+            return value;
+        }
+        public static bool CheckPowerOfTwo(int number)
+        {
+            return (number != 0) && ((number & (number - 1)) == 0);
         }
     }
 }
+
--- NunitTesting/Program.cs
+++ NunitTesting/Program.cs
@@ -10,9 +10,26 @@ namespace NunitTesting
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a decimal number: ");
-            int decimalNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Binary representation: " + BinaryConverter.ToBinary(decimalNumber));
+            //Console.Write("Enter an integer: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //string binary = BinaryConverter.ToBinary(number);
+            //Console.WriteLine("Binary: {0}", binary);
+
+            //int swapped = BinaryConverter.SwapNibbles(binary);
+            //Console.WriteLine("Swapped: {0}", swapped);
+
+            //bool isPowerOfTwo = BinaryConverter.CheckPowerOfTwo(swapped);
+            //Console.WriteLine("Is power of two: {0}", isPowerOfTwo);
+            //Console.ReadLine();
+
+            int decimalNumber = 100;
+            string binaryNumber = BinaryConverter.ToBinary(decimalNumber);
+            int swappedDecimalNumber = BinaryConverter.SwapNibbles(binaryNumber);
+
+            Console.WriteLine("Decimal number: " + decimalNumber);
+            Console.WriteLine("Binary number: " + binaryNumber);
+            Console.WriteLine("Swapped decimal number: " + swappedDecimalNumber);
             Console.ReadLine();
         }
     }
